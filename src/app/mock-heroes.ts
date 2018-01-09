9dd7121d4e48b9e2c7f12ae72c750ebae1fee102
DmitriyBalykin <dmitriy.a.balykin@gmail.com>
2018-01-09 11:12:44 +0200

    Initial commit: with services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+﻿import { Hero } from './hero';
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Venik' },
+    { id: 15, name: 'Bolt' },
+    { id: 16, name: 'Magma' },
+    { id: 17, name: 'Tomato' },
+    { id: 18, name: 'Magneta' },
+    { id: 19, name: 'Tornado' },
+    { id: 20, name: 'Ra' }
+];
